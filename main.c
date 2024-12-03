73c84d3f396d8143da79352b4dba9289ae483176
setayesh <nejadmohammad.setayesh@gmail.com>
2024-12-03 16:26:48 +0330

    madaret kharabe

--- main.c
+++ main.c
@@ -7,15 +7,18 @@ int main(void) {
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
     Texture2D duck= LoadTexture("D://FUM//projects//elysian//duck.png");
+    Texture2D table1 = LoadTexture("D://git projects//elysian//table1.jpg");
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         BeginDrawing();
+//kljwefasdnkjnaskfna,snf,asnfjasnfsnfm,n
 
         ClearBackground(GRAY);
 
-        DrawText("Elysian!", 850, 500, 80, BLACK);
+        //DrawText("Elysian!", 850, 500, 80, BLACK);
         //DrawTexture(duck,100,100,WHITE);
+        DrawTexture(table1,450, 20, WHITE);
 
         EndDrawing();
 
