a49714af4b795f391bb291ac4b99ebc554d1084a
Your Name <you@example.com>
2024-12-06 14:22:07 +0000

    add new changes draw.h draw.c completed

--- main.c
+++ main.c
@@ -1,56 +1,6 @@
 #include <stdio.h>
 #include "raylib.h"
-
-
-const int maxRow = 17;
-const int maxCol = 17;
-const int cellSize = 50;
-const int windowSize = 1050;
-int startX = (windowSize - (maxCol*cellSize)) / 2 + 400;
-int startY = (windowSize - (maxRow*cellSize)) / 2;
-
-
-//draw a table.
-void drawGrid(int n, int m){
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize, cellSize, LIGHTGRAY);
-            DrawRectangleLines(startX + j*cellSize, startY + i*cellSize, cellSize, cellSize, DARKGRAY);
-        }
-    }
-}
-
-void drawColor(int x, int y, int n, int m){    //x and y are chosen by user and this function will change the color of that position.
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    DrawRectangle(startX + x*cellSize, startY + y*cellSize, cellSize-2, cellSize-2, RED);
-}
-void drawCastle(int x, int y, int n, int m) {
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    Texture2D castle= LoadTexture("C://Users//M//CLionProjects//elysian1//pics//castle.png");
-    DrawTexture(castle, startX + x * cellSize, startY + y * cellSize, WHITE);
-}
-void drawVillage(int x, int y, int n, int m){
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    Texture2D village = LoadTexture("D://git projects//elysian//pics//village.png");
-    DrawTexture(village, startX + x * cellSize, startY + y * cellSize, WHITE);
-}
-void drawBlock(int x, int y, int n, int m){
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    Texture2D block = LoadTexture("C://Users//M//CLionProjects//elysian1//pics//blouk (2).png");
-    DrawTexture(block, startX + x * cellSize, startY + y * cellSize, WHITE);
-}
-
-
-
-
-
-
+#include "draw.h"
 
 int main(void) {
     //*********************************************************************************************************
@@ -141,10 +91,10 @@ int main(void) {
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-    Texture2D duck = LoadTexture("D://FUM//projects//elysian//duck.png");
-    Texture2D table1 = LoadTexture("D://FUM//projects//elysian//duck.png");
-    Texture2D village = LoadTexture("pics/village.png");
-    Texture2D castle = LoadTexture("pics/castle.png");
+    //Texture2D duck = LoadTexture("D://FUM//projects//elysian//duck.png");
+    //Texture2D table1 = LoadTexture("D://FUM//projects//elysian//duck.png");
+    //Texture2D village = LoadTexture("pics/village.png");
+    //Texture2D castle = LoadTexture("pics/castle.png");
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
@@ -157,6 +107,11 @@ int main(void) {
 
         //ClearBackground(RAYWHITE);
         drawGrid(n, m);
+        void drawGrid(int n, int m);
+        void drawColor(int x, int y, int n, int m); //x and y are chosen by user and this function will change the color of that position.
+        void drawCastle(int x, int y, int n, int m);
+        void drawVillage(int x, int y, int n, int m);
+        void drawBlock(int x, int y, int n, int m);
 
         //Draw the icons in the main window
         for (int i = 0; i < n; i++) {
