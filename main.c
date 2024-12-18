7436c34751595ae96d1f33b84d9322fce4329d1b
Setayesh <nejadmohammad.setayesh@gmail.com>
2024-12-18 21:18:09 +0330

    add Faz-2 branch

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include "raylib.h"
 #include "draw.h"
+#include "runaway.h"
 
 
 const int maxRow = 17;
@@ -12,6 +13,10 @@ const int maxCol = 17;
 //int startX = (windowSize - (maxCol*cellSize)) / 2 + 400;
 //int startY = (windowSize - (maxRow*cellSize)) / 2;
 
+/*typedef struct{
+    int x;
+    int y;
+}Point;*/
 
 
 
@@ -27,13 +32,36 @@ int main(void) {
     int map[17][17]={0};
     Map(map, n, m);
     generate_random(map, n, m);
+
+    Point start, end;
+
+    int startX = (windowSize - (m*cellSize)) / 2 + 400;
+    int startY = (windowSize - (n*cellSize)) / 2;
+
+    for(int i = 0; i<n ; i++){
+        for(int j = 0; j<m; j++){
+            start.x = i;
+            start.y = j;
+            if(map[i][j] == 'c'){
+                for(int k = 0; k < villNum; k++){
+                    end = villCordination[k];
+                    road(map, n, m, start, end, 1);
+                }
+            }
+            else if(map[i][j] == 'b'){
+                for(int k = 0; k < villNum; k++){
+                    end = villCordination[k];
+                    road(map, n, m, start, end, 2);
+                }
+            }
+        }
+    }
     //*********************************************************************************************************
     InitWindow(0, 0, "main screen");
 
     SetTargetFPS(120);               // Set our game to run at 60 frames-per-second
     // Seed the random number generator with the current time
     //srand(time(NULL));
-
 
     //--------------------------------------------------------------------------------------
 
@@ -54,42 +82,44 @@ int main(void) {
         //DrawText("Elysian!", 850, 20, 80, DARKBLUE);
 
         //Draw the icons in the main window
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m ; j++){
-                if(map[i][j] == 'c'){
+        for(int i = 0; i<n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 'c') {
                     drawCastle1(i, j, n, m);
                 }
-                else if(map[i][j] == 'b'){
+                else if (map[i][j] == 'b') {
                     drawCastle2(i, j, n, m);
                 }
-                else if(map[i][j] == 'v'){
+                else if (map[i][j] == 'v') {
                     drawVillage(i, j, n, m);
                 }
-                else if(map[i][j] == 'x'){
+                else if (map[i][j] == 'x') {
                     drawBlock(i, j, n, m);
                 }
-                else{
-                    Color level1 = (Color){0, 146, 115, 16 };
-                    Color level2 = (Color){0, 146, 115, 32} ;
-                    Color level3 = (Color){0, 146, 115, 64} ;
-                    Color level4 = (Color){0, 146, 115, 128} ;
-                    Color level5 = (Color){0, 146, 115, 255} ;
-                    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-                    int startY = (windowSize - (n*cellSize)) / 2;
-                    if(map[i][j]== 1){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level1);
-                    }
-                    else if(map[i][j]== 2){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level2);
-                    }
-                    else if(map[i][j]== 3){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level3);
-                    }
-                    else if(map[i][j]== 4){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level4);
-                    }
-                    else if(map[i][j]== 5){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level5);
+                else if (map[i][j] == 'r') {
+                    DrawRectangle(startX + i * cellSize, startY + j * cellSize, cellSize - 2, cellSize - 2, BROWN);
+                }
+                else if(map[i][j] == 'R'){
+                    DrawRectangle(startX + i * cellSize, startY + j * cellSize, cellSize - 2, cellSize - 2, PURPLE);
+                }
+                else {
+                    Color level1 = (Color) {0, 146, 115, 16};
+                    Color level2 = (Color) {0, 146, 115, 32};
+                    Color level3 = (Color) {0, 146, 115, 64};
+                    Color level4 = (Color) {0, 146, 115, 128};
+                    Color level5 = (Color) {0, 146, 115, 255};
+                    int startX = (windowSize - (m * cellSize)) / 2 + 400;
+                    int startY = (windowSize - (n * cellSize)) / 2;
+                    if (map[i][j] == 1) {
+                        DrawRectangle(startX + i * cellSize, startY + j * cellSize, cellSize - 2, cellSize - 2, level1);
+                    } else if (map[i][j] == 2) {
+                        DrawRectangle(startX + i * cellSize, startY + j * cellSize, cellSize - 2, cellSize - 2, level2);
+                    } else if (map[i][j] == 3) {
+                        DrawRectangle(startX + i * cellSize, startY + j * cellSize, cellSize - 2, cellSize - 2, level3);
+                    } else if (map[i][j] == 4) {
+                        DrawRectangle(startX + i * cellSize, startY + j * cellSize, cellSize - 2, cellSize - 2, level4);
+                    } else if (map[i][j] == 5) {
+                        DrawRectangle(startX + i * cellSize, startY + j * cellSize, cellSize - 2, cellSize - 2, level5);
                     }
                 }
             }
