18f5dc3130b8d5e01b233f863c169f96ef03bb11
Setayesh <nejadmohammad.setayesh@gmail.com>
2024-12-18 21:00:16 +0330

    add Faz-2 branch runaway.h

---
+++ runaway.h
@@ -0,0 +1,14 @@
+#ifndef ELYSIAN_RUNAWAY_H
+#define ELYSIAN_RUNAWAY_H
+
+// Function declarations
+bool bfs();
+void markPath();
+void printGrid();
+void road();
+
+// Variable declarations (if global)
+
+
+
+#endif //ELYSIAN_RUNAWAY_H
