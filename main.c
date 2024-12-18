c002298278eaedf632ea9bce03f6cbe75b93ff16
Setayesh <nejadmohammad.setayesh@gmail.com>
2024-12-18 14:08:07 +0330

    update something -_-

--- main.c
+++ main.c
@@ -2,79 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include "raylib.h"
+#include "draw.h"
 
 
 const int maxRow = 17;
 const int maxCol = 17;
-const int cellSize = 50;
-const int windowSize = 1050;
-int startX = (windowSize - (maxCol*cellSize)) / 2 + 400;
-int startY = (windowSize - (maxRow*cellSize)) / 2;
-
-
-//draw a table.
-void drawGrid(int n, int m){
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    Texture2D background = LoadTexture("D://git projects//elysian//pics//background.png");
-    DrawTexture(background,0,0,WHITE);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            //DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize, cellSize, LIGHTGRAY);
-            DrawRectangleLines(startX + j*cellSize, startY + i*cellSize, cellSize, cellSize, DARKGRAY);
-        }
-    }
-}
-
-void drawColor(int x, int y, int n, int m){    //x and y are chosen by user and this function will change the color of that position.
-    // Seed the random number generator with the current time
-    //srand(time(NULL));
-    int randomNum = (rand() % 5) + 1; //Generate a random number
-    Color level1 = (Color){0, 146, 115, 16 };
-    Color level2 = (Color){0, 146, 115, 32} ;
-    Color level3 = (Color){0, 146, 115, 64} ;
-    Color level4 = (Color){0, 146, 115, 128} ;
-    Color level5 = (Color){0, 146, 115, 255} ;
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    if(randomNum == 1){
-        DrawRectangle(startX + x*cellSize, startY + y*cellSize, cellSize-2, cellSize-2, level1);
-    }
-    else if(randomNum == 2){
-        DrawRectangle(startX + x*cellSize, startY + y*cellSize, cellSize-2, cellSize-2, level2);
-    }
-    else if(randomNum == 3){
-        DrawRectangle(startX + x*cellSize, startY + y*cellSize, cellSize-2, cellSize-2, level3);
-    }
-    else if(randomNum == 4){
-        DrawRectangle(startX + x*cellSize, startY + y*cellSize, cellSize-2, cellSize-2, level4);
-    }
-    else if(randomNum == 5){
-        DrawRectangle(startX + x*cellSize, startY + y*cellSize, cellSize-2, cellSize-2, level5);
-    }
-
-}
-void drawCastle(int x, int y, int n, int m) {
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    Texture2D castle= LoadTexture("D://git projects//elysian//pics//castle.png");
-    DrawTexture(castle, startX + x * cellSize, startY + y * cellSize, WHITE);
-}
-void drawVillage(int x, int y, int n, int m){
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    Texture2D village = LoadTexture("D://git projects//elysian//pics//village.png");
-    DrawTexture(village, startX + x * cellSize, startY + y * cellSize, WHITE);
-}
-void drawBlock(int x, int y, int n, int m){
-    int startX = (windowSize - (m*cellSize)) / 2 + 400;
-    int startY = (windowSize - (n*cellSize)) / 2;
-    Texture2D block = LoadTexture("D://git projects//elysian//pics//block.png");
-    DrawTexture(block, startX + x * cellSize, startY + y * cellSize, WHITE);
-}
-
-
-
+//const int cellSize = 50;
+//const int windowSize = 1050;
+//int startX = (windowSize - (maxCol*cellSize)) / 2 + 400;
+//int startY = (windowSize - (maxRow*cellSize)) / 2;
 
 
 
@@ -89,81 +25,8 @@ int main(void) {
     scanf("%d", &m); //get the table column num
 
     int map[17][17]={0};
-
-    printf("number of castles(1 or 2): ");
-    int castleNum, castle1, castle2;
-    scanf("%d", &castleNum);                //get the number of castles
-    int x1, y1, x2, y2;
-    if(castleNum==1){
-        printf("castle 1 coordinations(x1, y1):\n");        //get castles coordinates
-        printf("x1 = ");
-        scanf("%d", &x1);
-        printf("y1 = ");
-        scanf("%d", &y1);
-    }
-    else{
-        printf("castle 1 coordinations(x1, y1):\n");
-        printf("x1 = ");
-        scanf("%d", &x1);
-        printf("y1 = ");
-        scanf("%d", &y1);
-        printf("castle 2 coordinations(x2, y2):\n");
-        printf("x2 = ");
-        scanf("%d", &x2);
-        printf("y2 = ");
-        scanf("%d", &y2);
-    }
-
-    printf("Enter the number of village(max = 25): ");
-    int villNum;
-    scanf("%d", &villNum);
-    for(int i = 0; i<villNum; i++){
-        int x, y;
-        printf("Enter the coordination of village number %d: ", i+1);
-        scanf("%d %d", &x, &y);
-        map[x-1][y-1] = 'v';
-    }
-    int gold[25], food[25];
-    for(int i = 0; i<villNum; i++){
-        printf("Enter the gold and food for village number %d: ", i+1);
-        scanf("%d %d", &gold[i], &food[i]);
-    }
-
-    printf("Enter the number of block: ");
-    int blockNum;
-    scanf("%d", &blockNum);
-    for(int i = 0; i<blockNum; i++){
-        int x, y;
-        printf("Enter the coordanation of block number %d: ", i+1);
-        scanf("%d %d", &x, &y);
-        map[x-1][y-1] ='x';
-    }
-
-
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<m; j++){
-            if(i+1 == x1 && j+1 == y1){
-                //printf("c1 ");
-                map[i][j] = 'c';
-            }
-                /*else if(i == x2 && j == y2){
-                    printf("c2 ");
-                    map[i][j] = 'b';
-                }*/
-            else if(map[i][j] == 0){
-                //printf(" 1 ");
-                map[i][j] = 1;
-            }
-        }
-        printf("\n");
-    }
-    printf("\n");
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<m ; j++){
-            printf(" %d ", map[i][j]);
-        }
-        printf("\n");
-    }
+    Map(map, n, m);
+    generate_random(map, n, m);
     //*********************************************************************************************************
     InitWindow(0, 0, "main screen");
 
@@ -173,10 +36,7 @@ int main(void) {
 
 
     //--------------------------------------------------------------------------------------
-    Texture2D duck= LoadTexture("D://FUM//projects//elysian//duck.png");
-    Texture2D table1 = LoadTexture("D://FUM//projects//elysian//duck.png");
-    Texture2D village= LoadTexture("pics/village.png");
-    Texture2D castle= LoadTexture("pics/castle.png");
+
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
@@ -184,17 +44,23 @@ int main(void) {
 
         ClearBackground(GRAY);
 
-        //DrawText("Elysian!", 850, 20, 80, BLACK);
         //DrawTexture(duck,100,100,WHITE);
 
+
+
         //ClearBackground(RAYWHITE);
+        //draw table
         drawGrid(n,m);
+        //DrawText("Elysian!", 850, 20, 80, DARKBLUE);
 
         //Draw the icons in the main window
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m ; j++){
                 if(map[i][j] == 'c'){
-                    drawCastle(i, j, n, m);
+                    drawCastle1(i, j, n, m);
+                }
+                else if(map[i][j] == 'b'){
+                    drawCastle2(i, j, n, m);
                 }
                 else if(map[i][j] == 'v'){
                     drawVillage(i, j, n, m);
@@ -202,8 +68,29 @@ int main(void) {
                 else if(map[i][j] == 'x'){
                     drawBlock(i, j, n, m);
                 }
-                else {
-                    drawColor(i, j, n, m);
+                else{
+                    Color level1 = (Color){0, 146, 115, 16 };
+                    Color level2 = (Color){0, 146, 115, 32} ;
+                    Color level3 = (Color){0, 146, 115, 64} ;
+                    Color level4 = (Color){0, 146, 115, 128} ;
+                    Color level5 = (Color){0, 146, 115, 255} ;
+                    int startX = (windowSize - (m*cellSize)) / 2 + 400;
+                    int startY = (windowSize - (n*cellSize)) / 2;
+                    if(map[i][j]== 1){
+                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level1);
+                    }
+                    else if(map[i][j]== 2){
+                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level2);
+                    }
+                    else if(map[i][j]== 3){
+                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level3);
+                    }
+                    else if(map[i][j]== 4){
+                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level4);
+                    }
+                    else if(map[i][j]== 5){
+                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level5);
+                    }
                 }
             }
         }
