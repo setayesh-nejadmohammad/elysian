70fda4fcaeb4498cbf92632b0bf308b516dad5f8
Your Name <you@example.com>
2024-12-24 21:13:24 +0000

    new update

--- draw.h
+++ draw.h
@@ -1,6 +1,7 @@
 // draw.h
 #ifndef draw_H
 #define draw_H
+#include "info.h"
 
 // Function declarations (prototypes)
 void drawGrid();
@@ -13,8 +14,27 @@ void generate_random();
 void way();
 // ... other declarations
 
+
+// Struct declarations
+
+typedef struct{
+    int x;
+    int y;
+}Point;
+
+
+
 // Variable declarations (if global)
 extern int windowSize;  // Declare as extern to indicate it's defined elsewhere
 extern int cellSize;
+extern int n;
+extern int m;
+extern int villNum;
+extern int castleNum;
+extern int startX, startY;
+extern int maxRow, maxCol;
+extern int map[17][17];
+extern kingdom k1 , k2 ;
+extern village v[25];
 
 #endif // DRAW_H
