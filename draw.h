28c46657e968ffd48f36b2812a95569c2e664208
Setayesh <nejadmohammad.setayesh@gmail.com>
2024-12-24 15:55:27 +0330

    Add the way function draw.h

--- draw.h
+++ draw.h
@@ -13,9 +13,24 @@ void generate_random();
 void way();
 // ... other declarations
 
+// Struct declarations
+
+typedef struct{
+    int x;
+    int y;
+}Point;
+
+
+
 // Variable declarations (if global)
 extern int windowSize;  // Declare as extern to indicate it's defined elsewhere
 extern int cellSize;
+extern int n;
+extern int m;
+extern int villNum;
+extern int castleNum;
+extern int startX, startY;
+extern int maxRow, maxCol;
+extern int map[17][17];
 
 #endif // DRAW_H
-
