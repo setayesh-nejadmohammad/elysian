0346bc229128c737144e48f0a244e06285ebb767
Setayesh <nejadmohammad.setayesh@gmail.com>
2024-12-24 10:26:13 +0330

    make some variables GLOBAL

--- draw.h
+++ draw.h
@@ -16,6 +16,13 @@ void way();
 // Variable declarations (if global)
 extern int windowSize;  // Declare as extern to indicate it's defined elsewhere
 extern int cellSize;
+extern int n;
+extern int m;
+extern int villNum;
+extern int castleNum;
+extern int startX, startY;
+extern int maxRow, maxCol;
+extern int map[17][17];
 
 #endif // DRAW_H
 
