bd49b66d02fb4c408b5cf69d81b1a160e82eaf58
Setayesh <nejadmohammad.setayesh@gmail.com>
2024-12-28 15:19:10 +0330

    add the Round 1 2 3 functions draw.h

--- draw.h
+++ draw.h
@@ -1,6 +1,8 @@
 // draw.h
 #ifndef draw_H
 #define draw_H
+#include <stdbool.h>
+#include "info.h"
 
 // Function declarations (prototypes)
 void drawGrid();
@@ -11,7 +13,17 @@ void drawBlock();
 void Map();
 void generate_random();
 void way();
+void drawInformation();
 // ... other declarations
+
+// Struct declarations
+
+typedef struct{
+    int x;
+    int y;
+}Point;
+
+
 
 // Variable declarations (if global)
 extern int windowSize;  // Declare as extern to indicate it's defined elsewhere
@@ -23,5 +35,7 @@ extern int castleNum;
 extern int startX, startY;
 extern int maxRow, maxCol;
 extern int map[17][17];
+extern kingdom k1, k2;
+extern village v[25];
 
 #endif // DRAW_H
