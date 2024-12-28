c56e17792132bf85a12cbb5f491c259610a1db1b
Setayesh <nejadmohammad.setayesh@gmail.com>
2024-12-28 18:42:25 +0330

    Faz 3 draw.c

--- draw.c
+++ draw.c
@@ -5,6 +5,8 @@
 #include "info.h"
 
 
+
+
 const int maxRow = 17;
 const int maxCol = 17;
 
@@ -112,6 +114,9 @@ void Map() {
         printf("Enter the coordination of village number %d: ", i + 1);
         scanf("%d %d", &x, &y);
         map[x - 1][y - 1] = 'v';
+        v[i].x = x - 1;
+        v[i].y = y - 1;
+        v[i].free = true;
     }
 
     for (int i = 0; i < villNum; i++) {
@@ -153,12 +158,34 @@ void Map() {
     }
 }
 
+void wayCheck(int x,int y){
+    if(map[x][y]=='r'){
+        if(map[x-1][y]=='v' || map[x+1][y]=='v' || map[x][y-1]=='v' || map[x][y+1]=='v'){
+            for(int i=0;i<villNum;i++){
+                if(x-1==v[i].x && y==v[i].y || x+1==v[i].x && y==v[i].y || x==v[i].x && y-1==v[i].y || y+1==v[i].y && x==v[i].x){
+                    v[i].free = false;
+                    k1.goldProduction += v[i].goldRate;
+                    k1.foodProduction += v[i].foodRate;
+                }
+            }
+        }
+    }
+}
 void way(){
     int x, y;
     Vector2 mousePos = GetMousePosition();
     x = (mousePos.x-startX)/50;
     y = (mousePos.y-startY)/50;
-    map[x][y] = 'r';
+    if(map[x][y] != 'x' && map[x][y] != 'v' && map[x][y] != 'c' && map[x][y] != 'b'){
+        if(k1.worker >= map[x][y]){
+            k1.worker -= map[x][y];
+            map[x][y] = 'r';
+            wayCheck(x, y);
+        }
+        else{
+            DrawText("You don't have enough worker!", 650, 820, 40, RED);
+        }
+    }
 }
 
 void drawInformation(int Round){
@@ -186,4 +213,19 @@ void drawInformation(int Round){
     sprintf(s, "%d", k1.soldier);
     DrawText("k1.soldier: ", 10, 500, 40, DARKBLUE);
     DrawText(s, 210, 500, 40, DARKBLUE);
+
+    sprintf(s, "%d", k1.goldProduction);
+    DrawText("k1.gold Production: ", 10, 600, 40, DARKBLUE);
+    DrawText(s, 390, 600, 40, DARKBLUE);
+
+    sprintf(s, "%d", k1.foodProduction);
+    DrawText("k1.food Production: ", 10, 700, 40, DARKBLUE);
+    DrawText(s, 400, 700, 40, DARKBLUE);
+
+    //.............................  Round Guide  ..........................................
+    DrawText("key 1  >> buyFood ",1600,60,30,DARKBLUE);
+    DrawText("key 2 >> getWorker ",1600,90,30,DARKBLUE);
+    DrawText("key 3 >> getSoldier ",1600,120,30,DARKBLUE);
+    DrawText("click on the cell you want >> makeRoad",1300,30,30,DARKBLUE);
+    DrawText("key 4 >> doNothing ",1600,150,30,DARKBLUE);
 }
