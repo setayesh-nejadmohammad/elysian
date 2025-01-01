566cd7c10d3e20e67d07e8b23ab1e7f18466749f
Setayesh <nejadmohammad.setayesh@gmail.com>
2025-01-01 15:09:46 +0330

    Faz 3 (enshala!) draw.h

--- draw.h
+++ draw.h
@@ -1,6 +1,8 @@
 // draw.h
 #ifndef draw_H
 #define draw_H
+#include <stdbool.h>
+#include "info.h"
 
 // Function declarations (prototypes)
 void drawGrid();
@@ -10,8 +12,15 @@ void drawVillage();
 void drawBlock();
 void Map();
 void generate_random();
-void way();
+bool Way();
+void drawInformation();
 // ... other declarations
+
+// Struct declarations
+
+
+
+
 
 // Variable declarations (if global)
 extern int windowSize;  // Declare as extern to indicate it's defined elsewhere
@@ -23,6 +32,8 @@ extern int castleNum;
 extern int startX, startY;
 extern int maxRow, maxCol;
 extern int map[17][17];
+extern kingdom k1, k2;
+extern village v[25];
+extern int UntakenVills;
 
 #endif // DRAW_H
-
