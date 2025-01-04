835c533b0cf2e992b1b57b4dfad98ab0f44514d8
Setayesh <nejadmohammad.setayesh@gmail.com>
2025-01-04 18:02:59 +0330

    add map1 & map2 draw.c

--- draw.c
+++ draw.c
@@ -14,12 +14,18 @@ const int maxCol = 17;
 
 int n; //Number of Rows
 int m; //Number of Column
-int map[17][17] = {0};
+
+int map[17][17] = {0};  //main map
+int map1[17][17] = {0}; //k1 map
+int map2[17][17] = {0}; //k2 map
+
 int villNum; //Number of villages
 int castleNum;
 int startX;
 int startY;
 int UntakenVills;
+
+int Round = 1;
 
 kingdom k1, k2;
 village v[25];
@@ -54,8 +60,8 @@ void generate_random(){
 }
 
 void drawCastle1(int y, int x) {
-    Texture2D castle= LoadTexture("D://git projects//elysian//pics//castle.png");
-    DrawTexture(castle, startX + x * cellSize, startY + y * cellSize, PURPLE);
+    Texture2D castle= LoadTexture("D://git projects//elysian//pics//castle2.png");
+    DrawTexture(castle, startX + x * cellSize, startY + y * cellSize, DARKBLUE);
 }
 void drawCastle2(int y, int x) {
     Texture2D castle= LoadTexture("D://git projects//elysian//pics//castle2.png");
@@ -81,6 +87,8 @@ void Map() {
     k1.goldProduction = 1;
     k1.foodProduction = 0;
     k1.roadCount = 0;
+    k1.villNum = 0;
+
 
     k2.gold = 5;
     k2.worker = 1;
@@ -90,6 +98,7 @@ void Map() {
     k2.foodProduction = 0;
     k2.roadCount = 0;
     k2.x = k2.y = -1;
+    k2.villNum = 0;
 
     printf("number of castles(1 or 2): ");
     int castle1, castle2;
@@ -223,109 +232,120 @@ void Map() {
     }
 }
 
+void roadClear(int x, int y, int state){ // state shows that which Kingdom won the war
+    int rCount = 0; // number of road around the first road
+    if(map1[x-1][y] == 'r'){
+        rCount++;
+    }
+    if(map1[x][y-1] == 'r'){
+        rCount++;
+    }
+    if(map1[x+1][y] == 'r'){
+        rCount++;
+    }
+    if(map1[x][y+1] == 'r'){
+        rCount++;
+    }
+    if(rCount > 1){
+        map1[x][y] = map[x][y];  //remove the road from map1
+    }
+}
+
 void wayCheck(int x,int y){
-    if(map[x][y]=='r'){
-        if(map[x-1][y]=='v' || map[x+1][y]=='v' || map[x][y-1]=='v' || map[x][y+1]=='v'){
-            for(int i=0;i<villNum;i++){
-                if((x-1==v[i].x && y==v[i].y || x+1==v[i].x && y==v[i].y || x==v[i].x && y-1==v[i].y || y+1==v[i].y && x==v[i].x ) && v[i].free != false){
-                    v[i].free = false;
-                    UntakenVills -= 1;
-                    k1.goldProduction += v[i].goldRate;
-                    k1.foodProduction += v[i].foodRate;
+    if(Round % 2 != 0){   //k1
+        if(map1[x][y]=='r'){
+            if(map1[x-1][y]=='v' || map1[x+1][y]=='v' || map1[x][y-1]=='v' || map1[x][y+1]=='v'){
+                for(int i=0;i<villNum;i++){
+                    if((x-1==v[i].x && y==v[i].y || x+1==v[i].x && y==v[i].y || x==v[i].x && y-1==v[i].y || y+1==v[i].y && x==v[i].x ) && v[i].free != false){
+                        v[i].free = false;
+                        k1.vills[i] = i;
+                        k1.villNum ++;
+                        UntakenVills -= 1;
+                        k1.goldProduction += v[i].goldRate;
+                        k1.foodProduction += v[i].foodRate;
+                    }
                 }
             }
         }
     }
+    else{   //k2
+        if(map2[x][y]=='R'){
+            if(map2[x-1][y]=='v' || map2[x+1][y]=='v' || map2[x][y-1]=='v' || map2[x][y+1]=='v'){
+                for(int i=0;i<villNum;i++){
+                    if((x-1==v[i].x && y==v[i].y || x+1==v[i].x && y==v[i].y || x==v[i].x && y-1==v[i].y || y+1==v[i].y && x==v[i].x ) && v[i].free != false){
+                        v[i].free = false;
+                        k2.vills[i] = i;
+                        k2.villNum ++;
+                        UntakenVills -= 1;
+                        k2.goldProduction += v[i].goldRate;
+                        k2.foodProduction += v[i].foodRate;
+                    }
+                }
+            }
+        }
+    }
+
 }
 bool Way(){
     int x, y;
     Vector2 mousePos = GetMousePosition();
     y = (mousePos.x-startX)/50;
     x = (mousePos.y-startY)/50;
-    if(x == k1.x + 1 && y == k1.y || x == k1.x && y == k1.y + 1 || x == k1.x - 1 && y == k1.y || x == k1.x && y == k1.y - 1
-    || map[x][y-1] == 'r' || map[x-1][y] == 'r' || map[x+1][y] == 'r' || map[x][y+1] == 'r'){
-        if(map[x][y] != 'x' && map[x][y] != 'v' && map[x][y] != 'c' && map[x][y] != 'b'){
-            if(k1.worker >= map[x][y]){
-                k1.road[k1.roadCount].x = x;
-                k1.road[k1.roadCount].y = y;
-                k1.roadCount++;
-                //k1.worker -= map[x][y];
-                map[x][y] = 'r';
-                wayCheck(x, y);
+    if(Round % 2 != 0){    //k1 way
+        bool flag = false;
+        for(int i = 0; i < k1.villNum; i++){
+            if(x-1==v[k1.vills[i]].x && y==v[k1.vills[i]].y || x+1==v[k1.vills[i]].x && y==v[k1.vills[i]].y || x==v[k1.vills[i]].x && y-1==v[k1.vills[i]].y || y+1==v[k1.vills[i]].y && x==v[k1.vills[i]].x ){
+                flag = true;
+                break;
             }
-            else if(map[x][y] != 'r'){
-                map[x][y] -= k1.worker;
-            }
-            return true;
         }
+        if(x == k1.x + 1 && y == k1.y || x == k1.x && y == k1.y + 1 || x == k1.x - 1 && y == k1.y || x == k1.x && y == k1.y - 1
+           || map1[x][y-1] == 'r' || map1[x-1][y] == 'r' || map1[x+1][y] == 'r' || map1[x][y+1] == 'r' || flag){
+            if(map1[x][y] != 'x' && map1[x][y] != 'v' && map1[x][y] != 'c' && map1[x][y] != 'b' && map1[x][y] != 'R'){
+                if(k1.worker >= map1[x][y]){
+                    k1.road[k1.roadCount].x = x;
+                    k1.road[k1.roadCount].y = y;
+                    k1.roadCount++;
+                    map1[x][y] = 'r';
+                    map2[x][y] = 'r';
+                    wayCheck(x, y);
+                }
+                else if(map1[x][y] != 'r'){
+                    map1[x][y] -= k1.worker;
+                }
+                return true;
+            }
+        }
+
+        return false;
     }
+    else{     //k2 way
+        bool flag = false;
+        for(int i = 0; i < k2.villNum; i++){
+            if(x-1==v[k2.vills[i]].x && y==v[k2.vills[i]].y || x+1==v[k2.vills[i]].x && y==v[k2.vills[i]].y || x==v[k2.vills[i]].x && y-1==v[k2.vills[i]].y || y+1==v[k2.vills[i]].y && x==v[k2.vills[i]].x ){
+                flag = true;
+                break;
+            }
+        }
+        if(x == k2.x + 1 && y == k2.y || x == k2.x && y == k2.y + 1 || x == k2.x - 1 && y == k2.y || x == k2.x && y == k2.y - 1
+           || map2[x][y-1] == 'R' || map2[x-1][y] == 'R' || map2[x+1][y] == 'R' || map2[x][y+1] == 'R' || flag){
+            if(map2[x][y] != 'x' && map2[x][y] != 'v' && map2[x][y] != 'c' && map2[x][y] != 'b' && map2[x][y] != 'r'){
+                if(k2.worker >= map2[x][y]){
+                    k2.road[k2.roadCount].x = x;
+                    k2.road[k2.roadCount].y = y;
+                    k2.roadCount++;
+                    map2[x][y] = 'R';
+                    map1[x][y] = 'R';
+                    wayCheck(x, y);
+                }
+                else if(map2[x][y] != 'R'){
+                    map2[x][y] -= k2.worker;
+                }
+                return true;
+            }
+        }
 
-    return false;
+        return false;
+    }
 }
 
-void drawInformation(int Round) {
-
-    //..........................  Round Counter .....................................................
-    char RoundS[5];
-    sprintf(RoundS, "%d", Round);  // Convert int to string
-    DrawText("Round: ", 800, 20, 40, DARKBLUE);
-    DrawText(RoundS, 950, 20, 40, DARKBLUE);
-    //................................................................................................
-
-    //.......................... kingdom 1 information ...............................................
-    char s[5];
-    sprintf(s, "%d", k1.food);
-    DrawText("k1.food: ", 10, 200, 40, DARKBLUE);
-    DrawText(s, 210, 200, 40, DARKBLUE);
-
-    sprintf(s, "%d", k1.gold);
-    DrawText("k1.gold: ", 10, 300, 40, DARKBLUE);
-    DrawText(s, 210, 300, 40, DARKBLUE);
-
-    sprintf(s, "%d", k1.worker);
-    DrawText("k1.worker: ", 10, 400, 40, DARKBLUE);
-    DrawText(s, 210, 400, 40, DARKBLUE);
-
-    sprintf(s, "%d", k1.soldier);
-    DrawText("k1.soldier: ", 10, 500, 40, DARKBLUE);
-    DrawText(s, 210, 500, 40, DARKBLUE);
-
-    sprintf(s, "%d", k1.goldProduction);
-    DrawText("k1.gold Production: ", 10, 600, 40, DARKBLUE);
-    DrawText(s, 390, 600, 40, DARKBLUE);
-
-    sprintf(s, "%d", k1.foodProduction);
-    DrawText("k1.food Production: ", 10, 700, 40, DARKBLUE);
-    DrawText(s, 400, 700, 40, DARKBLUE);
-    //.................................................................................................
-
-
-    //.....................................  Round Guide  ..............................................
-    DrawText("key 1  >> buyFood ", 1600, 60, 30, DARKBLUE);
-    DrawText("key 2 >> getWorker ", 1600, 90, 30, DARKBLUE);
-    DrawText("key 3 >> getSoldier ", 1600, 120, 30, DARKBLUE);
-    DrawText("click on the cell you want >> makeRoad", 1300, 30, 30, DARKBLUE);
-    DrawText("key 4 >> doNothing ", 1600, 150, 30, DARKBLUE);
-    //...................................................................................................
-
-    //....................... Color introduction (difficulty level) .....................................
-    Color level1 = (Color){0, 146, 115, 16};
-    Color level2 = (Color){0, 146, 115, 32};
-    Color level3 = (Color){0, 146, 115, 64};
-    Color level4 = (Color){0, 146, 115, 128};
-    Color level5 = (Color){0, 146, 115, 255};
-    DrawText("Hardest", 1550, 450, 30, DARKBLUE);
-    DrawRectangle(1600, 520, cellSize-2, cellSize-2, level5);
-    DrawRectangle(1600, 570, cellSize-2, cellSize-2, level4);
-    DrawRectangle(1600, 620, cellSize-2, cellSize-2, level3);
-    DrawRectangle(1600, 670, cellSize-2, cellSize-2, level2);
-    DrawRectangle(1600, 720, cellSize-2, cellSize-2, level1);
-    DrawText("Easiest", 1550, 770, 30, DARKBLUE);
-    DrawText("Number of\nneeded workers", 1700, 450, 27, DARKBLUE);
-    DrawText(" --->  5", 1650, 520, 30, DARKBLUE);
-    DrawText(" --->  4", 1650, 570, 30, DARKBLUE);
-    DrawText(" --->  3", 1650, 620, 30, DARKBLUE);
-    DrawText(" --->  2", 1650, 670, 30, DARKBLUE);
-    DrawText(" --->  1", 1650, 720, 30, DARKBLUE);
-    //................................................................................................
-}
