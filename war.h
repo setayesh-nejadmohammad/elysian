00745cdd73fc3c5038c544cb5ace3d7b79592781
Setayesh <nejadmohammad.setayesh@gmail.com>
2025-01-05 20:25:36 +0330

    add the war.h library

---
+++ war.h
@@ -0,0 +1,14 @@
+//
+// Created by setayesh on 1/5/2025.
+//
+
+#ifndef ELYSIAN_WAR_H
+#define ELYSIAN_WAR_H
+#include <stdbool.h>
+
+void roadClear();
+
+extern bool mark[17][17];
+extern bool validRoad[17][17];
+
+#endif //ELYSIAN_WAR_H
