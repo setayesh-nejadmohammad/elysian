e7c7ba43f918fa6470c1998cdafcc334c8c3c7c5
Your Name <you@example.com>
2025-01-05 20:52:58 +0000

    add new library for faz-4 (war) war.h

---
+++ war.h
@@ -0,0 +1,12 @@
+#ifndef ELYSIAN_WAR_H
+#define ELYSIAN_WAR_H
+#include <stdbool.h>
+#include "info.h"
+#include "draw.h"
+
+bool war();
+void roadClear();
+void dfs();
+void checkRoad();
+
+#endif //ELYSIAN_WAR_H
