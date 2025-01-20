1a8c7dd98c1605bc33717c4b4ff19d4a0843cc68
Setayesh <nejadmohammad.setayesh@gmail.com>
2025-01-20 14:10:11 +0330

    Faz-5 main.c

--- main.c
+++ main.c
@@ -1,46 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "raylib.h"
 #include "draw.h"
+#include "info.h"
+#include <string.h>
 
-
-
-
-
+// in all the maps:
+// v = not taken village
+// V = taken village by k1
+// W = taken village by k2
 
 
 int main(void) {
     //*********************************************************************************************************
-    //int n, m;
-    printf("Enter the dimensions of the game:(it will be a n*m table)\n");
-    printf("n = ");
-    scanf("%d", &n); //get the table row num
-    printf("m = ");
-    scanf("%d", &m); //get the table column num
 
-    //int map[17][17]={0};
     Map();
     generate_random();
+    for(int i = 0; i < n; i++){      // Fill map1 and map2 with map
+        for(int j = 0; j < m; j++){
+            map1[i][j] = map[i][j];
+            map2[i][j] = map[i][j];
+        }
+    }
     //*********************************************************************************************************
     InitWindow(0, 0, "main screen");
 
     SetTargetFPS(120);               // Set our game to run at 60 frames-per-second
-    // Seed the random number generator with the current time
-    //srand(time(NULL));
 
+    //Texture2D RoundGuide = LoadTexture("D:/git projects/elysian/pics/RoundGuide.png");
+    Texture2D background = LoadTexture("D://git projects//elysian//pics//background.png");
+    Texture2D block = LoadTexture("D://git projects//elysian//pics//block.png");
+    Texture2D village = LoadTexture("D://git projects//elysian//pics//village2.png");
+    Texture2D castle = LoadTexture("D://git projects//elysian//pics//castle2.png");
 
     //--------------------------------------------------------------------------------------
 
+    bool endGame = true;
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose() && endGame)    // Detect window close button or ESC key
     {
         BeginDrawing();
 
         ClearBackground(GRAY);
 
+
+        DrawTexture(background, 0, 0, WHITE);
+
         //DrawTexture(duck,100,100,WHITE);
 
+        if(UntakenVills == -1) {
+            if(WhoWin == 1){
+                DrawText("YOU WIN!", 500, 400, 200, DARKBLUE);
+            }
+            else if(WhoWin == 2){
+                DrawText("YOU WIN!", 500, 400, 200, DARKPURPLE);
+            }
+            EndDrawing();
+            WaitTime(3);
+            endGame = false;
+        }
 
 
         //ClearBackground(RAYWHITE);
@@ -48,51 +66,172 @@ int main(void) {
         drawGrid();
         //DrawText("Elysian!", 850, 20, 80, DARKBLUE);
 
+
+
         //Draw the icons in the main window
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m ; j++){
-                //printf("%d ", map[i][j]);
-                if(map[i][j] == 'c'){
-                    drawCastle1(i, j);
-                }
-                else if(map[i][j] == 'b'){
-                    drawCastle2(i, j);
-                }
-                else if(map[i][j] == 'v'){
-                    drawVillage(i, j);
-                }
-                else if(map[i][j] == 'x'){
-                    drawBlock(i, j);
-                }
-                else{
-                    Color level1 = (Color){0, 146, 115, 16 };
-                    Color level2 = (Color){0, 146, 115, 32} ;
-                    Color level3 = (Color){0, 146, 115, 64} ;
-                    Color level4 = (Color){0, 146, 115, 128} ;
-                    Color level5 = (Color){0, 146, 115, 255} ;
-                    if(map[i][j]== 1){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level1);
+        if(Round % 2 != 0){ //.............................. k1 ..................................
+            for(int i = 0; i<n; i++){
+                for(int j = 0; j<m ; j++){
+                    if(map1[i][j] == 'c'){
+                        //drawCastle1(i, j);
+                        DrawTexture(castle, startX + j * cellSize, startY + i * cellSize, DARKBLUE);
                     }
-                    else if(map[i][j]== 2){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level2);
+                    else if(map1[i][j] == 'b'){
+                        //drawCastle2(i, j);
+                        DrawTexture(castle, startX + j * cellSize, startY + i * cellSize, DARKPURPLE);
                     }
-                    else if(map[i][j]== 3){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level3);
+                    else if(map1[i][j] == 'v'){
+                        //drawVillage(i, j);
+                        DrawTexture(village, startX + j * cellSize, startY + i * cellSize, BLACK);
                     }
-                    else if(map[i][j]== 4){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level4);
+                    else if(map1[i][j] == 'V'){
+                        //drawVillage1(i, j);
+                        DrawTexture(village, startX + j * cellSize, startY + i * cellSize, DARKBLUE);
                     }
-                    else if(map[i][j]== 5){
-                        DrawRectangle(startX + i*cellSize, startY + j*cellSize, cellSize-2, cellSize-2, level5);
+                    else if(map1[i][j] == 'W'){
+                        //drawVillage2(i, j);
+                        DrawTexture(village, startX + j * cellSize, startY + i * cellSize, DARKPURPLE);
+                    }
+                    else if(map1[i][j] == 'x'){
+                        //drawBlock(i, j);
+                        DrawTexture(block, startX + j * cellSize, startY + i * cellSize, WHITE);
+                    }
+                    else if(map1[i][j] == 'r'){
+                        DrawRectangle(startX + j * cellSize, startY + i * cellSize, cellSize - 2, cellSize - 2, DARKBLUE);
+                    }
+                    else if(map1[i][j] == 'R'){
+                        DrawRectangle(startX + j * cellSize, startY + i * cellSize, cellSize - 2, cellSize - 2, DARKPURPLE);
+                    }
+                    else{
+                        Color level1 = (Color){0, 146, 115, 16};
+                        Color level2 = (Color){0, 146, 115, 32};
+                        Color level3 = (Color){0, 146, 115, 64};
+                        Color level4 = (Color){0, 146, 115, 128};
+                        Color level5 = (Color){0, 146, 115, 255};
+                        if(map1[i][j]== 1){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level1);
+                        }
+                        else if(map1[i][j]== 2){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level2);
+                        }
+                        else if(map1[i][j]== 3){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level3);
+                        }
+                        else if(map1[i][j]== 4){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level4);
+                        }
+                        else if(map1[i][j]== 5){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level5);
+                        }
                     }
                 }
             }
-            //printf("\n");
+        }
+        else {  //.......................................... k2 ..........................................
+            for(int i = 0; i<n; i++){
+                for(int j = 0; j<m ; j++){
+                    if(map2[i][j] == 'c'){
+                        //drawCastle1(i, j);
+                        DrawTexture(castle, startX + j * cellSize, startY + i * cellSize, DARKBLUE);
+                    }
+                    else if(map2[i][j] == 'b'){
+                        //drawCastle2(i, j);
+                        DrawTexture(castle, startX + j * cellSize, startY + i * cellSize, DARKPURPLE);
+                    }
+                    else if(map2[i][j] == 'v'){
+                        //drawVillage(i, j);
+                        DrawTexture(village, startX + j * cellSize, startY + i * cellSize, BLACK);
+                    }
+                    else if(map2[i][j] == 'V'){
+                        //drawVillage1(i, j);
+                        DrawTexture(village, startX + j * cellSize, startY + i * cellSize, DARKBLUE);
+                    }
+                    else if(map2[i][j] == 'W'){
+                        //drawVillage2(i, j);
+                        DrawTexture(village, startX + j * cellSize, startY + i * cellSize, DARKPURPLE);
+                    }
+                    else if(map2[i][j] == 'x'){
+                        //drawBlock(i, j);
+                        DrawTexture(block, startX + j * cellSize, startY + i * cellSize, WHITE);
+                    }
+                    else if(map2[i][j] == 'r'){
+                        DrawRectangle(startX + j * cellSize, startY + i * cellSize, cellSize - 2, cellSize - 2, DARKBLUE);
+                    }
+                    else if(map2[i][j] == 'R'){
+                        DrawRectangle(startX + j * cellSize, startY + i * cellSize, cellSize - 2, cellSize - 2, DARKPURPLE);
+                    }
+                    else{
+                        Color level1 = (Color){0, 146, 115, 16};
+                        Color level2 = (Color){0, 146, 115, 32};
+                        Color level3 = (Color){0, 146, 115, 64};
+                        Color level4 = (Color){0, 146, 115, 128};
+                        Color level5 = (Color){0, 146, 115, 255};
+                        if(map2[i][j]== 1){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level1);
+                        }
+                        else if(map2[i][j]== 2){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level2);
+                        }
+                        else if(map2[i][j]== 3){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level3);
+                        }
+                        else if(map2[i][j]== 4){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level4);
+                        }
+                        else if(map2[i][j]== 5){
+                            DrawRectangle(startX + j*cellSize, startY + i*cellSize, cellSize-2, cellSize-2, level5);
+                        }
+                    }
+                }
+            }
         }
 
-        EndDrawing();
 
+        drawInformation();  //define in info.c
+
+
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+            //way(); // for Faz 4 we will add a MOUSE_BUTTON_RIGHT for second castle
+            if(Way()){
+                update();
+                Round++;
+            }
+            else{
+                DrawText("You can't make road here!", 650, 820, 40, RED);
+                EndDrawing();
+                WaitTime(2);
+            }
+        }
+
+
+
+        if(IsKeyPressed(KEY_ONE)){
+            buyFood();
+            update();
+            Round++;
+        }
+        if(IsKeyPressed(KEY_TWO)){
+            getWorker();
+            update();
+            Round++;
+        }
+        if(IsKeyPressed(KEY_THREE)){
+            getSoldier();
+            update();
+            Round++;
+        }
+        if(IsKeyPressed(KEY_FOUR)){ //This Round will do NOTHING. :/
+            update();
+            Round++;
+        }
+
+
+
+
+        EndDrawing();
     }
+
+
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
@@ -101,4 +240,3 @@ int main(void) {
 
     return 0;
 }
-
